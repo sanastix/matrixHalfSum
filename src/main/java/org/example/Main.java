127dd084c427f2c3690f48f30bc4c362a6d06750
susanna.stepanian <sanastepanyan@gmail.com>
2023-09-21 21:18:12 +0300

    optimized code

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,57 +1,46 @@
 package org.example;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.PrintWriter;
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws FileNotFoundException {
-
-        Scanner in = new Scanner(new File("input.txt"));
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new FileReader("input.txt"));
         PrintWriter out = new PrintWriter("output.txt");
-        int n = in.nextInt();
-        int m = in.nextInt();
+
+        StringTokenizer st = new StringTokenizer(in.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
         int[][] input = new int[n][m];
 
         for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(in.readLine());
             for (int j = 0; j < m; j++) {
-                input[i][j] = in.nextInt();
+                input[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-        in.close();
-
         int[][] output = new int[n][m];
-        output[0][0] = input[0][0];
-        out.print(output[0][0] + " ");
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                if (i == 0) {
-                    if (j == 0) continue;
-                    output[i][j] = input[i][j] + output[i][j-1];
-                    out.print(output[i][j] + " ");
-                } else {
-                    if (j == 0){
-                        output[i][j] = input[i][j] + output[i-1][j];
-                        out.print(output[i][j] + " ");
-                    } else {
-                        output[i][j] = input[i][j] + output[i][j-1];
-                        int tmp = i-1;
-                        while (tmp >= 0){
-                            output[i][j] += input[tmp][j];
-                            --tmp;
-                        }
-                        out.print(output[i][j] + " ");
-                    }
+                int sum = input[i][j];
+                if (i > 0) {
+                    sum += output[i - 1][j];
                 }
+                if (j > 0) {
+                    sum += output[i][j - 1];
+                }
+                if (i > 0 && j > 0) {
+                    sum -= output[i - 1][j - 1];
+                }
+                output[i][j] = sum;
+                out.print(sum + " ");
             }
-            out.print("\n");
+            out.println();
         }
 
+        in.close();
         out.close();
-
     }
-
 }
